6491ac8c82d68779890fdd828f75759f9b9eab25
annar <p4nd4@posteo.de>
2023-01-09 13:23:36 +0100

    move javascript to its own file

--- miesmuschelscript.js
+++ miesmuschelscript.js
@@ -1,62 +1,86 @@
 //mit switch:
 
+//const bild = document.getElementById("bild1"); 
 
-var bild = document.getElementById("bild1"); 
+( function () {
+
+    document.getElementById("sendQuestion").onclick = function () {
+        miesmuschelSagt();
+    };
+
+    const hierText = document.getElementById("hierText");
+    const bild = document.getElementById("bild1");
 
 function miesmuschelSagt() {
-    var hiertext = document.getElementById("hiertext");
-    let zufallswert = Math.floor(Math.random() * 3);
-switch (zufallswert) {
-    case 0 :  
-        return "gewiss";
-        //bild = imglink("http://i.giphy.com/mWToZdKc0sl4k.gif")
-        break;
-    case 1 :  
-        return  "egal";
-        break;
-    case 2 :  
-        return  "nein";
-        break;
-    case 3 :  
-        return  "frag doch nocheinmal";
-        break;
-    case 4 :  
-        return  "selbstverständlich";
-        break;
-    case 5 :  
-        return  "aber sicher";
-        break;
-    case 6 :  
-        return  "frag mich was leichteres";
-            break;
-    case 7 :  
-        return  "gewiss";
-        break;
-    case 8 :  
-        return  "gewiss";
-        break;
-    case 9 :  
-        return  "gewiss";
-        break;
-    case 10 :  
-        return  "gewiss";
-        break;
-    case 11 :  
-        return  "gewiss";
-        break;
-    default:
-        console.log("iwas funzt nicht im switch")
-        break;
-}
-}
 
-console.log(miesmuschelSagt())
+
+   
+    let zufallswert = Math.floor(Math.random() * 11);
+    switch (zufallswert) {
+        case 0 :
+            bild.innerHTML = "<img src='http://i.giphy.com/mWToZdKc0sl4k.gif'></img>"
+            hierText.innerText = "gewiss";
+            break;
+        case 1 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/NNgLaLxRYgYUw.gif'></img>"
+            hierText.innerText = "egal";
+            break;
+        case 2 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/p3Tlnu6jY1cNq.gif'></img>"
+            hierText.innerText = "nein";
+            break;
+        case 3 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/NWg7M1VlT101W.gif'></img>"
+            hierText.innerText = "frag doch nocheinmal";
+            break;
+        case 4 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/JawaXj8kyWJ44.gif'></img>"
+            hierText.innerText = "selbstverständlich";
+            break;
+        case 5 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/YFis3URdQJ6qA.gif'></img>"
+            hierText.innerText = "aber sicher";
+            break;
+        case 6 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/l41lV04RvMyCysJX2.gif'></img>"
+            hierText.innerText = "frag mich was leichteres";
+                break;
+        case 7 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/O3GqAYR9jFxLi.gif'></img>"
+            hierText.innerText = "ich glaube eher nicht";
+            break;
+        case 8 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/3o7TKxZPj7mbWpinWo.gif'></img>"
+            hierText.innerText = "ich weiß es nicht";
+            break;
+        case 9 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/YlitvKufE2O4M.gif'></img>"
+            hierText.innerText = "keineswegs";
+            break;
+        case 10 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/9w3w4eP5ZUgiQ.gif'></img>"
+            hierText.innerText = "eines tages vielleicht";
+            break;
+        case 11 :  
+            bild.innerHTML = "<img src='http://i.giphy.com/9w3w4eP5ZUgiQ.gif'></img>"
+            hierText.innerText = "ja";
+            break;
+        default:
+            bild.innerHTML = "<img src='http://i.giphy.com/NNgLaLxRYgYUw.gif'></img>"
+            console.log("iwas funzt nicht im switch")
+            break;
+    }
+    
+ }
+
+miesmuschelSagt();
+})();
 
 /*
 var state = 0;
 
 function imglink(src){
-    return "<img src='" + src + "'></img>"    
+    hierText.innerText ="<img src='" + src + "'></img>"    
 }
 
 function setstate(){
@@ -71,45 +95,45 @@ function setstate(){
 function myfunction(){
  //   |  | 
  //  letzteres |  |  | willst du das wirklich wissen |  |
-    var hiertext = document.getElementById("hiertext");
+    var hierText = document.getElementById("hierText");
     var bild = document.getElementById("bild1"); 
 
     if (state == 0){
-        hiertext.innerText = "gewiss"
+        hierText.innerText = "gewiss"
         bild.innerHTML = imglink("http://i.giphy.com/mWToZdKc0sl4k.gif")
     } else if (state == 1){
-        hiertext.innerText = "egal"
+        hierText.innerText = "egal"
         bild.innerHTML = imglink("http://i.giphy.com/NNgLaLxRYgYUw.gif")
     } else if (state == 2){
-        hiertext.innerText = "nein"
+        hierText.innerText = "nein"
         bild.innerHTML = imglink("http://i.giphy.com/p3Tlnu6jY1cNq.gif")
     } else if (state == 3){
-        hiertext.innerText = "frag doch nocheinmal"
+        hierText.innerText = "frag doch nocheinmal"
         bild.innerHTML = imglink("http://i.giphy.com/NWg7M1VlT101W.gif")
     } else if (state == 4){
-        hiertext.innerText = "selbstverständlich"
+        hierText.innerText = "selbstverständlich"
         bild.innerHTML = imglink("http://i.giphy.com/JawaXj8kyWJ44.gif") 
     } else if (state == 5){
-        hiertext.innerText = "aber sicher"
+        hierText.innerText = "aber sicher"
         bild.innerHTML = imglink("http://i.giphy.com/YFis3URdQJ6qA.gif") 
     } else if (state == 6){
-        hiertext.innerText = "frag mich was leichteres"
+        hierText.innerText = "frag mich was leichteres"
         bild.innerHTML = imglink("http://i.giphy.com/l41lV04RvMyCysJX2.gif") 
     } else if (state == 7){
-        hiertext.innerText = "ich glaube eher nicht"
+        hierText.innerText = "ich glaube eher nicht"
         bild.innerHTML = imglink("http://i.giphy.com/O3GqAYR9jFxLi.gif") 
     } else if (state == 8){
-        hiertext.innerText = "ich weiß es nicht"
+        hierText.innerText = "ich weiß es nicht"
         bild.innerHTML = imglink("http://i.giphy.com/3o7TKxZPj7mbWpinWo.gif") 
     } else if (state == 9){
-        hiertext.innerText = "keineswegs"
+        hierText.innerText = "keineswegs"
         bild.innerHTML = imglink("http://i.giphy.com/YlitvKufE2O4M.gif") 
     } else if (state == 10){
-        hiertext.innerText = "eines tages vielleicht"
+        hierText.innerText = "eines tages vielleicht"
         bild.innerHTML = imglink("http://i.giphy.com/9w3w4eP5ZUgiQ.gif") 
 
     } else {
-        hiertext.innerText = "ja"
+        hierText.innerText = "ja"
         bild.innerHTML = imglink("http://i.giphy.com/9w3w4eP5ZUgiQ.gif")
     }
     
